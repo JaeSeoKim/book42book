08102806c90a685922d8cf81fe3032e11d871614
Likilee <gnsdlrl@daum.net>
2020-12-17 00:38:13 +0900

    Main v1

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,18 @@
-import React from "react"
-import tw from "twin.macro";
+import React from 'react';
+import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-const Search = () => {
-	return (<>search1</>)
+const Search = (props) => {
+  return (
+    <FormGroup>
+        <Label for="Search"></Label>
+        <Input
+          type="search"
+          name="search"
+          id="Search"
+          placeholder="도서 검색"
+        />
+      </FormGroup>
+  );
 }
 
-export default Search
+export default Search;
