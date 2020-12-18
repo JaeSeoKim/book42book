e663cdcc0b5ee4d7097444b0102df7b9ae4506ae
jaeskim <jaeskim@student.42seoul.kr>
2020-12-18 11:05:36 +0900

    fix npm start

--- src/db.js
+++ src/db.js
@@ -1,21 +1,20 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import mongoose from "mongoose";
+const dev = process.env.NODE_ENV !== "production";
+if (dev) require("dotenv").config();
 
-dotenv.config()
-
-const MONGO_URL = process.env.MONGO_URL
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
-  useFindAndModify: false
-})
+  useFindAndModify: false,
+});
 
 mongoose.Promise = global.Promise;
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-const handleOpen = () => console.log('✅ DB Connection Succes!')
-const handleClose = error => console.log(`❎ DB Connection Fail : ${error}`)
+const handleOpen = () => console.log("✅ DB Connection Succes!");
+const handleClose = (error) => console.log(`❎ DB Connection Fail : ${error}`);
 
-db.once('open', handleOpen)
-db.on('error', handleClose)
+db.once("open", handleOpen);
+db.on("error", handleClose);
